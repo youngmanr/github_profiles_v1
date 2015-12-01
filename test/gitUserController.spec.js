78c677586f811840e2be0a80c7a35337c99fa927
Robert Youngman <rob_youngman@yahoo.com>
2015-12-01 15:05:57 +0000

    doSearch funtion added

--- test/gitUserController.spec.js
+++ test/gitUserController.spec.js
@@ -18,7 +18,7 @@ describe('GitUserSearchController', function() {
         "login": "tansaku",
         "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
         "html_url": "https://github.com/tansaku"
-      }, 
+      },
       {
         "login": "stephenlloyd",
         "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
@@ -27,6 +27,8 @@ describe('GitUserSearchController', function() {
     ];
 
     it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
